901726fd3239e641d6c7740400579d109d999ecc
Annie Yin <anniesy2@uci.edu>
2026-08-14 18:54:26 -0700

    feat: implement early stopping via rolling averages

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ import torch
 
 from agents import DQNAgent
 from replay_buffer import ReplayBuffer
+import utils
 
 
 ENV_NAME = "LunarLander-v3"
@@ -34,15 +35,22 @@ def run_baseline(env, episodes, max_steps, seed):
     return results
 
 
-def train_agent(env, episodes, max_steps, seed, batch_size, learning_starts):
+def train_agent(env, episodes, max_steps, seed, batch_size, learning_starts, stop_after=10):
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.n
 
     agent = DQNAgent(state_dim=state_dim, action_dim=action_dim)
     buffer = ReplayBuffer()
     results = []
+    steps_no_improv = 0
+    AVG_REWARD_WINDOW = 10
+    curr_avg = None
+    lthreshold_ep = -1
 
     for episode in range(1, episodes + 1):
+        if lthreshold_ep != -1 and episode - lthreshold_ep >= AVG_REWARD_WINDOW:
+            curr_avg = utils.calculate_avg_rewards(results[-AVG_REWARD_WINDOW:])
+
         state, _ = env.reset(seed=seed + episode)
         total_reward = 0
 
@@ -54,6 +62,8 @@ def train_agent(env, episodes, max_steps, seed, batch_size, learning_starts):
             buffer.store_transition(state, action, reward, next_state, done)
 
             if buffer.size >= learning_starts:
+                if lthreshold_ep == -1:
+                    lthreshold_ep = episode
                 batch = buffer.sample(batch_size)
                 agent.update(batch)
 
@@ -74,6 +84,14 @@ def train_agent(env, episodes, max_steps, seed, batch_size, learning_starts):
 
         if episode % 25 == 0:
             print_summary("Training", results)
+
+        if curr_avg is not None and curr_avg > utils.calculate_avg_rewards(results[-AVG_REWARD_WINDOW:]):
+            steps_no_improv += 1
+        else:
+            steps_no_improv = 0
+
+        if steps_no_improv >= stop_after:
+            break
 
     # torch.save(agent.q_net.state_dict(), "model.pth")
     # print("Model saved to model.pth")
--- utils.py
+++ utils.py
@@ -1,13 +1,7 @@
-def calculate_avg_n_rewards(results, n):
-    # gets the last n reward values from a set of results and averages them
-    
-    if len(results) < n:
-        raise IndexError(f'Cannot calculate average for {n} results because results is smaller than {n}.')
-
-    result_set = results[-10, -1]
+def calculate_avg_rewards(results):
     reward_sum = 0
 
-    for result in enumerate(result_set):
-        reward_sum += result_set.get("return")
+    for result in results:
+        reward_sum += result.get("return")
 
-    return reward_sum / n
+    return reward_sum / len(results)
